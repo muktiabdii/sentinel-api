1381b855d4fd68d04644f7dbbb496785ad90bde5
muktiabdii <abdisyukur10@gmail.com>
2025-11-19 13:48:03 +0700

    feat(vercel): adding routes/index.js and refactor app.js for deploy in vercel

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('./src/config/env');
 
 const express = require('express');
-const authRoutes = require('./src/routes/authRoutes');
+const routes = require('./src/routes/index');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use('/api', routes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
