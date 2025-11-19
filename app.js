3183a243e5d302ff49f5109740f48701b185e63f
muktiabdii <abdisyukur10@gmail.com>
2025-11-19 19:32:40 +0700

    fix(vercel): fixing error in vercel.json and api not found

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ const express = require('express');
 const routes = require('./src/routes/index');
 
 const app = express();
-
 app.use(express.json());
 
 app.use('/api', routes);
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
