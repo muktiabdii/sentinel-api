78dcb1f267893fc4998961dd584075df049b2ef1
Dzvelocity <dzikrimurtadlo@gmail.com>
2025-11-21 19:03:39 +0700

    add test endpoint

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const uploadController = require('../controllers/uploadController');
 
+router.get('/test', (req, res) => {
+  res.json({ message: "Route Upload Berhasil Terbaca!", status: "OK" });
+});
+
 router.get('/signature', uploadController.getUploadSignature);
 
 module.exports = router;
