f1ed4b581fb8ec5faa4c8d561ee9b27939e51a3b
Dzvelocity <dzikrimurtadlo@gmail.com>
2025-11-21 19:06:11 +0700

    check for uploadRoutes endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,23 @@ const router = express.Router();
 const authRoutes = require('./authRoutes');
 const productRoutes = require('./productRoutes');
 const transactionRoutes = require('./transactionRoutes');
-const uploadRoutes = require('./uploadRoutes');
+// const uploadRoutes = require('./uploadRoutes');
 
 router.get('/', (req, res) => {
   res.send('✅ EventEase API running');
 });
 
+// --- TES JALUR TIKUS (LANGSUNG DISINI) ---
+router.get('/upload/cek-jalur', (req, res) => {
+  res.json({ 
+    status: "Jalur Utama Aman!", 
+    info: "Router index.js berfungsi" 
+  });
+});
+
 router.use('/auths', authRoutes);
 router.use('/products', productRoutes);
 router.use('/transactions', transactionRoutes);
-router.use('/upload', uploadRoutes);
+// router.use('/upload', uploadRoutes);
 
 module.exports = router;
