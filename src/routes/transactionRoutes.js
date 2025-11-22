a7f5288126f009ea063af24676539df80758870b
muktiabdii <abdisyukur10@gmail.com>
2025-11-22 19:44:58 +0700

    refactor(transaction): refactor for authMiddleware in transactionRoute, declarating midtrans-notifications without authMiddleware

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -4,11 +4,9 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.use(authMiddleware);
-
-router.post('/', transactionController.create);       
-router.get('/', transactionController.getHistory);      
-router.get('/:id', transactionController.getDetail);
+router.post('/', authMiddleware, transactionController.create);       
+router.get('/', authMiddleware, transactionController.getHistory);      
+router.get('/:id', authMiddleware, transactionController.getDetail);
 router.post('/midtrans-notification', transactionController.midtransNotification);
 
 module.exports = router;
