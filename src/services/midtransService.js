587ac010e271a7230202547def4d3a2d9d69b54b
muktiabdii <abdisyukur10@gmail.com>
2025-11-22 16:39:37 +0700

    deleting log key midtrans

--- src/services/midtransService.js
+++ src/services/midtransService.js
@@ -1,13 +1,6 @@
 const midtransClient = require('midtrans-client');
 const env = require('../config/env');
 
-console.log("MIDTRANS SNAP CONFIG =", {
-  isProduction: env.midtrans.isProduction,
-  serverKey: env.midtrans.serverKey,
-  clientKey: env.midtrans.clientKey
-});
-
-
 const snap = new midtransClient.Snap({
     isProduction: env.midtrans.isProduction,
     serverKey: env.midtrans.serverKey,
